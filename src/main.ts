08e925272f4041a5b528b501e74d2375dcf474a1
matcher9131 <masch9131cd@gmail.com>
2023-11-02 09:08:05 +0900

    Fix main (Implement goToPreviousGame and goToNextGame)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "./index.css";
 import { createPositionEvents } from "./states/positionEvent";
 import { setTileAnimationAll, updateTileState } from "./states/tileState";
 import { type MJson } from "./types/Mjson/mJson";
+import { upperBound } from "./util/arrayExtensions";
 
 const root = document.getElementById("root");
 if (root == null) throw new Error("ERROR: 'root' is not found.");
@@ -27,10 +28,50 @@ const handleGoToNextPosition = (): void => {
     );
 };
 const handleGoToPreviousGame = (): void => {
-    // NOT IMPLEMENTED
+    const gameIndex = upperBound(gameBeginningPositions, positionIndex) - 1;
+    switch (positionIndex) {
+        case 0:
+            positionIndex = positionEvents.length - 1;
+            break;
+        case 1:
+            positionIndex = 0;
+            break;
+        case positionEvents.length - 1:
+            positionIndex = gameBeginningPositions[gameBeginningPositions.length - 1];
+            break;
+        case gameBeginningPositions[gameIndex]:
+            positionIndex = gameBeginningPositions[gameIndex - 1];
+            break;
+        default:
+            positionIndex = gameBeginningPositions[gameIndex];
+            break;
+    }
+    setTileAnimationAll(false);
+    updateTileState(
+        positionEvents[positionIndex].tileStateTransitions.filter((transition) => transition.kind === "forward"),
+    );
 };
 const handleGoToNextGame = (): void => {
-    // NOT IMPLEMENTED
+    if (positionIndex === positionEvents.length - 1) {
+        positionIndex = 0;
+    } else {
+        const gameIndex = upperBound(gameBeginningPositions, positionIndex) - 1;
+        switch (gameIndex) {
+            case -1:
+                positionIndex = 1;
+                break;
+            case gameBeginningPositions.length - 1:
+                positionIndex = positionEvents.length - 1;
+                break;
+            default:
+                positionIndex = gameBeginningPositions[gameIndex + 1];
+                break;
+        }
+    }
+    setTileAnimationAll(false);
+    updateTileState(
+        positionEvents[positionIndex].tileStateTransitions.filter((transition) => transition.kind === "forward"),
+    );
 };
 
 const board = createBoard();
