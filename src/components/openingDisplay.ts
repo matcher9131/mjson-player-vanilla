fa67d221f638191d14e84124fa36999135ae5c50
matcher9131 <masch9131cd@gmail.com>
2023-11-26 11:42:15 +0900

    Fix components/openingDisplay (Add each player's wind)

--- src/components/openingDisplay.ts
+++ src/components/openingDisplay.ts
@@ -4,20 +4,30 @@ import { rotateVector2D } from "@/util/vector2D";
 
 const openingDisplayId = "opening_display";
 const playerNameTextId = (sideIndex: number): string => `opening_display_player_name${sideIndex}`;
+const windIconId = (sideIndex: number): string => `opening_display_wind_icon${sideIndex}`;
+const windSrcId = (windIndex: number): string =>
+    "winds.svg#" + ["wind_east", "wind_south", "wind_west", "wind_north"][windIndex % 4];
 
 export const createOpeningDisplay = (): SVGGElement => {
     const element = document.createElementNS(svgNS, "g");
     element.setAttribute("id", openingDisplayId);
     for (let sideIndex = 0; sideIndex < 4; ++sideIndex) {
         const { x, y } = rotateVector2D({ x: 0, y: boardOneSize / 4 }, -90 * sideIndex);
-        const text = createSVGTextElement({
+        const textElement = createSVGTextElement({
             text: "",
             x,
-            y,
+            y: y - 350,
             fontSize: 400,
         });
-        text.setAttribute("id", playerNameTextId(sideIndex));
-        element.appendChild(text);
+        textElement.setAttribute("id", playerNameTextId(sideIndex));
+        element.appendChild(textElement);
+
+        const iconElement = document.createElementNS(svgNS, "use");
+        iconElement.setAttribute("id", windIconId(sideIndex));
+        iconElement.setAttribute("x", `${x}`);
+        iconElement.setAttribute("y", `${y + 350}`);
+        iconElement.setAttribute("href", windSrcId(sideIndex));
+        element.appendChild(iconElement);
     }
     // 初期状態
     element.setAttribute("opacity", "0");
